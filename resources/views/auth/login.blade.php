0a4716e747160f754ab851ca1a5444383ebdcd09
KIMMYY <kimmedrano25@gmail.com>
2026-01-06 17:05:24 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login | NAS SAIS</title>
     
-    <link rel="icon" href="{{ asset('images/nas/nas-logo-spotlight.jpg') }}">
+    <link rel="icon" href="/images/nas/nas-logo-spotlight.jpg">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800&display=swap" rel="stylesheet" />
