97c92f547fd2575536cb27feefcafaeb80943503
KIMMYY <kimmedrano25@gmail.com>
2026-01-06 15:32:55 +0800

    Fix SSL error by forcing cacert upload

--- config/database.php
+++ config/database.php
@@ -8,6 +8,12 @@ return [
     |--------------------------------------------------------------------------
     | Default Database Connection Name
     |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the database connections below you wish
+    | to use as your default connection for database operations. This is
+    | the connection which will be utilized unless another connection
+    | is explicitly specified when you execute a query / statement.
+    |
     */
 
     'default' => env('DB_CONNECTION', 'sqlite'),
@@ -16,6 +22,11 @@ return [
     |--------------------------------------------------------------------------
     | Database Connections
     |--------------------------------------------------------------------------
+    |
+    | Below are all of the database connections defined for your application.
+    | An example configuration is provided for each database system which
+    | is supported by Laravel. You're free to add / remove connections.
+    |
     */
 
     'connections' => [
@@ -43,9 +54,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            // ITO ANG FIX: Pilitin ang SSL connection para tanggapin ng TiDB Cloud
+            // IMPORTANT: Ito ang pipilit sa Vercel na gamitin ang cacert.pem para sa SSL
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => base_path('cacert.pem'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
@@ -96,6 +107,8 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
+            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
+            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
@@ -104,6 +117,11 @@ return [
     |--------------------------------------------------------------------------
     | Migration Repository Table
     |--------------------------------------------------------------------------
+    |
+    | This table keeps track of all the migrations that have already run for
+    | your application. Using this information, we can determine which of
+    | the migrations on disk haven't actually been run on the database.
+    |
     */
 
     'migrations' => [
@@ -115,6 +133,11 @@ return [
     |--------------------------------------------------------------------------
     | Redis Databases
     |--------------------------------------------------------------------------
+    |
+    | Redis is an open source, fast, and advanced key-value store that also
+    | provides a richer body of commands than a typical key-value system
+    | such as Memcached. You may define your connection settings here.
+    |
     */
 
     'redis' => [
