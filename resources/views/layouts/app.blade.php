97b78ce93279baf7ffd028e77f8a1392d08996a9
KIMMYY <kimmedrano25@gmail.com>
2026-01-07 08:40:26 +0800

    naglagay ng favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'NAS SAIS') }}</title>
 
+        <link rel="icon" type="image/jpeg" href="/images/nas/favicon.jpg"> 
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
