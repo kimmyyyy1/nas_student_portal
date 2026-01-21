2e98209da6b0a2b197a3a9858ea92f58dd0a3fa0
KIMMYY <kimmedrano25@gmail.com>
2026-01-21 16:34:14 +0800

    fixing remove button

--- resources/views/subjects/create.blade.php
+++ resources/views/subjects/create.blade.php
@@ -6,12 +6,6 @@
         * { font-family: 'Poppins', sans-serif !important; }
     </style>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Add New Subject') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg border border-gray-200">
